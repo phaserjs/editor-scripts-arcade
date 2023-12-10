0b1f0f4e5bd24a9beb09b5b715e850f37e4c5b9c
PhaserEditor2D <developers@phasereditor2d.com>
2023-12-10 07:47:57 -0500

    Updates build browser.

--- browser/phasereditor2d_scripts_arcade/lib/phasereditor2d_scripts_arcade.d.ts
+++ browser/phasereditor2d_scripts_arcade/lib/phasereditor2d_scripts_arcade.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="./matter" />
-class ArcadeObjectScript extends ScriptNode {
+declare class ArcadeObjectScript extends ScriptNode {
     constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene);
     get arcadeObject(): Phaser.GameObjects.GameObject & {
         body: Phaser.Physics.Arcade.Body;
@@ -7,7 +7,7 @@ class ArcadeObjectScript extends ScriptNode {
     get body(): Phaser.Physics.Arcade.Body | (MatterJS.BodyType & Phaser.Physics.Arcade.Body) | (Phaser.Physics.Arcade.StaticBody & Phaser.Physics.Arcade.Body);
 }
 
-class OnWorldBoundsCollideEventScript extends OnEventScript {
+declare class OnWorldBoundsCollideEventScript extends OnEventScript {
     constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene);
     up: boolean;
     down: boolean;
@@ -16,20 +16,20 @@ class OnWorldBoundsCollideEventScript extends OnEventScript {
     executeChildren(body: Phaser.Physics.Arcade.Body, up: boolean, down: boolean, left: boolean, right: boolean): void;
 }
 
-class SetVelocityActionScript extends ArcadeObjectScript {
+declare class SetVelocityActionScript extends ArcadeObjectScript {
     constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene);
     velocityX: number;
     velocityY: number;
     execute(...args: any[]): void;
 }
 
-class SetVelocityXActionScript extends ArcadeObjectScript {
+declare class SetVelocityXActionScript extends ArcadeObjectScript {
     constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene);
     velocityX: number;
     execute(...args: any[]): void;
 }
 
-class SetVelocityYActionScript extends ArcadeObjectScript {
+declare class SetVelocityYActionScript extends ArcadeObjectScript {
     constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene);
     velocityY: number;
     execute(...args: any[]): void;
